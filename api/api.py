9fc1beb31e717e2cf6af852af6a4cd5829c9363f
Jey <jeykumar3@users.noreply.github.com>
2021-01-16 00:44:14 -0500

    Create api.py

---
+++ api/api.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import random
+
+UPLOAD_EXTENSIONS = ['.docx']
+
+app = Flask(__name__)
+CORS(app)
+DEBUG = True
+
+
+@app.route('/get_word', methods=['GET'])
+def get_word():
+    """End point to get next word and related info."""
+    words = ['apple',
+             'banana',
+             'orange']
+    phonemes = [['AE1', 'P', 'AH0', 'L'],
+                ['B', 'AH0', 'N', 'AE1', 'N', 'AH0'],
+                ['AO1', 'R', 'AH0', 'N', 'JH']]
+    idx = random.randrange(len(words))
+    data = {'word': words[idx],
+            'phonemes': phonemes[idx]}
+    return jsonify(data)
+
+
+@app.route('/submit_results', methods=['POST'])
+def submit_results():
+    """Endpoint to send word pronunciation results."""
+    data = {'msg': 'success'}
+    return jsonify(data)
+
+
+if __name__ == '__main__':
+    app.run(host='', port=8001)
